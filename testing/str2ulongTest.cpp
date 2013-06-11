6dfb38df4e3c9ce3503a756654eb52e85211cf84
Marcel Schneider <marcel@coopmasters.de>
2013-06-11 07:01:38 +0200

    renamed a test for str2ulong

--- testing/str2ulongTest.cpp
+++ testing/str2ulongTest.cpp
@@ -5,7 +5,7 @@ CPPUNIT_TEST_SUITE_REGISTRATION (str2ulongTest);
 void str2ulongTest::setUp(){}
 void str2ulongTest::tearDown(){}
 
-void str2ulongTest::negative_value()
+void str2ulongTest::fails_on_negative_value()
 {
     inputstring.assign("-1");
     CPPUNIT_ASSERT (0 == csv2xls::str2ulong(inputstring.c_str(),num,10));
