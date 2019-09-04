0f02ec2352bf5e90c7f62dca1acfa51fa1fb25f5
Marcel Schneider <marcel@coopmasters.de>
2019-09-05 00:01:27 +0200

    remove useless code

--- csv2xlslib.Test/funcompTest.cpp
+++ csv2xlslib.Test/funcompTest.cpp
@@ -21,45 +21,5 @@ namespace funcompTest
         ~Tracer(){std::cout << "~Tracer()\n";}
     };
 
-    struct Data
-    {
-        int file;
-        Tracer tracer;
-    };
-
-
-    auto innerFunc1 = [](Data data)->std::optional<Data>{
-        if (data.file == 3)
-        {
-            return data;
-        }
-        return std::nullopt;
-    };
-
-    auto innerFunc2(int i)
-    {
-        return [&](std::optional<Data> out)->int{
-            if (out->file == 2 && i != '@')
-                return i;
-            return i+1;
-
-        };
-    }
-
-    auto isEnd = [](int i){
-        return i == '@';
-    };
-
-    TEST_CASE("lol")
-    {
-//        std::stringstream  file;
-//        file << "12345";
-//        std::vector<char> out;
-//        auto myfun =  innerFunc1|innerFunc2(2)|repeatUntil(isEnd);
-//
-//        myfun(Data{});
-//        REQUIRE(out == std::vector<char>{'1','2','3','4','5'});
-    }
-
 }
 }
